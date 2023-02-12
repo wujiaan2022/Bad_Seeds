1150b27a03dd8e308017f2b9d27043c56227471a
wujiaan2022 <emmawu2004@gmail.com>
2023-02-12 14:21:15 +0000

    add computer random guess funtion

--- run.py
+++ run.py
@@ -10,12 +10,13 @@ class Board:
     """
     def __init__(self, size, num_bad_seeds, name, type):
         self.size = size
-        self.board = [[" . " for x in range(size)] for y in range(size)]
+        self.board = [["o " for x in range(size)] for y in range(size)]
         self.num_bad_seeds = num_bad_seeds
         self.name = name
         self.type = type
         self.guess_place = []
-        self.add_place = []   
+        self.add_place = []  
+        self.rand_place = []      
     
     def print(self):
         """
@@ -32,45 +33,64 @@ class Board:
             add_coord = [add_row, add_col]
             if add_coord not in self.add_place:
                 self.add_place.append([add_row, add_col])
-                self.board[add_row][add_col] = " @ " 
+                self.board[add_row][add_col] = "@ " 
                                   
-    def my_guess(self):        
-        print("Please guess a row number:")        
-        while True:
-            try:
-                guess_row = int(input("Guess a number in 0,1,2,3,4  "))       
-                if guess_row < 5:            
-                    break        
-                elif guess_row > 4:
-                    print("Your number must be between 0 to 4.")                   
-            except ValueError:                 
-                print("You can only enter a interger number!")
+    def my_guess(self): 
 
-        print("\nPlease guess a column number:")        
-        while True:
-            try:
-                guess_col = int(input("Guess a number in 0,1,2,3,4  "))        
-                if guess_col < 5:            
-                    break        
-                elif guess_col > 4:
-                    print("Your number must be between 0 to 4.")                   
-            except ValueError:                 
-                print("You can only enter a interger number!")
-        self.guess_place.append([guess_row, guess_col]) 
-        if [guess_row, guess_col] in self.add_place:       
+        while True:                  
+            while True:
+                print("Please guess a row number:") 
+                try:
+                    guess_row = int(input("Guess a number in 0,1,2,3,4  "))       
+                    if guess_row < 5:            
+                        break        
+                    elif guess_row > 4:
+                        print("\nYour number must be between 0 to 4.")                   
+                except ValueError:                 
+                    print("\nYou can only enter a interger number!")
+                    
+            while True:
+                print("\nPlease guess a column number:")
+                try:
+                    guess_col = int(input("Guess a number in 0,1,2,3,4  "))        
+                    if guess_col < 5:            
+                        break        
+                    elif guess_col > 4:
+                        print("\nYour number must be between 0 to 4.")                   
+                except ValueError:                 
+                    print("\nYou can only enter a interger number!")
+
+            guess_coord = [guess_row, guess_col]
+            if guess_coord in self.guess_place:
+                print("You have already guessed that place, \nplease choose a different coordinate.")        
+            else:
+                break
+   
+        guess_coord = [guess_row, guess_col]  
+        self.guess_place.append(guess_coord) 
+        if guess_coord in self.add_place:       
             self.board[guess_row][guess_col] = " Y "
-            print("You got it! Thank you neighbor! \nNow I can plant something nice.") 
+            print("\nYou got it! Thank you neighbor! \nNow I can plant something nice.") 
         else:
             self.board[guess_row][guess_col] = " X "
-            print("You missed, but donot give up, keep going!")
+            print("\nYou missed, but donot give up, \nkeep going!")
+            self.score
+        print(f"Your current score is {}")
 
-    def neighbor_guess(self):
+    
+    def neighbor_rand_guess(self):
         rand_row = randint(0, self.size-1)
-        rand_col = randint(0, self.size-1)            
-        if [rand_row, rand_col] in self.add_place:
+        rand_col = randint(0, self.size-1)
+        rand_coord = [rand_row, rand_col]
+        if rand_coord not in self.rand_place:
+            self.rand_place.append([rand_row, rand_col])            
+        if rand_coord in self.add_place:
             self.board[rand_row][rand_col] = " Y " 
         else:
             self.board[rand_row][rand_col] = " X "
+
+
+
     
 
 
@@ -103,19 +123,15 @@ def new_game():
     neighbor_board.print()    
 
     neighbor_board.my_guess()
+    player_board.neighbor_rand_guess() 
+    print(f"{play_name}")   
+    player_board.print()
+    print("neighbor")
     neighbor_board.print()   
     
     
     
 new_game()
-
-
-
-
-
-
-    
     
 
     
-new_game()
